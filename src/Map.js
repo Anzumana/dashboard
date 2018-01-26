420d69cb5cd1bc32b14bf1c728ba40ac823ce269
Anzumana Taal <anzumana.taal@me.com>
2018-01-26 16:36:36 +0100

    feat: arcgis react component integration D4UM-248

---
+++ src/Map.js
@@ -0,0 +1,18 @@
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Scene } from 'react-arcgis';
+import BermudaTriangle from './BermudaTriangle';
+
+export default (props) => (
+		<Scene
+				style={{ width: '100vw', height: '100vh' }}
+				mapProperties={{ basemap: 'satellite' }}
+				viewProperties={{
+						center: [-79, 28],
+						zoom:8 
+				}}
+		>
+		<BermudaTriangle />
+		</Scene>
+)
