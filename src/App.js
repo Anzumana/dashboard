9a650045e0efd7326b403fe4ea7bfd8c8db2c474
Anzumana Taal <anzumana.taal@me.com>
2018-02-05 13:51:03 +0100

    feat: sidebar D4UM-248

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import SimpleLineChart from './components/SimpleLineChart'
 import SimpleTreemap from './components/SimpleTreemap'
 import MakeMap from './components/Map.js'
 import myScene  from './components/Scene'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 
-const Header = () => 
+const myHeader = () => 
 (
 	<Link to='/'> 
 	<h1> D4UM Dashbaord </h1>
@@ -27,20 +28,64 @@ const Home = ()=>
 	</div>
 )
 
-const App =() =>  (
-	<Router>
-	<div>
-		<Route path="/" children={Header}/>
-		<Route exact path="/" component={Home}/>
-		<Route path="/about" component={About}/>
-		<Route path="/bubbleChart" component={BubbleChart}/>
-		<Route path="/SimpleLineChart" component={SimpleLineChart}/>
-		<Route path="/SimpleTreemap" component={SimpleTreemap}/>
-		<Route path="/MakeMap" component={MakeMap}/>
-		<Route path="/myScene" component={myScene}/>
-		</div>
-	</Router>
-		
-);
 
+class App extends Component {
+  state = { visible: false }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  render() {
+			const { visible } = this.state
+    return (
+				<div >
+								<Router>
+								<div style={{height:  +100 +'vh'}}>
+					<Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
+					<Sidebar.Pushable as={Segment}>
+						<Sidebar as={Menu} animation='overlay' direction='right' visible={visible} icon='labeled'  vertical inverted>
+							<Menu.Item name='home'>
+								<Icon name='cube' />
+								<Link to='/'>Dashboard
+								</Link>
+							</Menu.Item>
+							<Menu.Item name='barchart'>
+								<Icon name='bar chart' />
+								<Link to='/SimpleTreemap'>Treemap</Link><br/>
+							</Menu.Item>
+							<Menu.Item name='camera'>
+								<Icon name='line chart' />
+								<Link to='/SimpleLineChart'>LineChart</Link> <br/>
+							</Menu.Item>
+							<Menu.Item name='camera'>
+								<Icon name='calendar' />
+								<Link to='/bubbleChart'>BubbleChart</Link> <br/>
+							</Menu.Item>
+							<Menu.Item name='globe'>
+								<Icon name='globe' />
+								<Link to='/MakeMap'>Globe</Link> <br/>
+							</Menu.Item>
+							<Menu.Item name='map'>
+								<Icon name='map' />
+								<Link to='/myScene'>Map</Link><br/>
+							</Menu.Item>
+						</Sidebar>
+						<Sidebar.Pusher>
+							<Segment basic>
+									<Route path="/" children={Header}/>
+									<Route exact path="/" component={Home}/>
+									<Route path="/about" component={About}/>
+									<Route path="/bubbleChart" component={BubbleChart}/>
+									<Route path="/SimpleLineChart" component={SimpleLineChart}/>
+									<Route path="/SimpleTreemap" component={SimpleTreemap}/>
+									<Route path="/MakeMap" component={MakeMap}/>
+									<Route path="/myScene" component={myScene}/>
+							</Segment>
+						</Sidebar.Pusher>
+					</Sidebar.Pushable>
+									</div>
+								</Router>
+				</div>
+    )
+  }
+}
 export default App;
