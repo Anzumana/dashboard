4a857e989dc2000e0c8b17e58d5df83a7149d89e
Anzumana Taal <anzumana.taal@me.com>
2018-05-25 16:32:47 +0200

    feat: sample request D4UM-422

--- src/lib/service.js
+++ src/lib/service.js
@@ -6,9 +6,9 @@ export const fetchResults=() =>{
 	var header = new Headers();
 	header.append('Authorization', 'Basic ' + 'D4UM'+ ':' + 'D4uM$2017' );
 	console.log('fetchResults');
-	return fetch('https:d4umnode.hannit.de',{
+	return fetch('https://d4umnode.hannit.de/events?location=9.59969,52.42556-9.82954,52.33702&since=2017-12-05T19:30:00&until=2017-12-05T19:30:00&type=null',{
 		header:header,
-		mode:'no-cors'
+		mode:'no-cors',
+		credentials:'include'
 	}).then(res=>res.json());
-		//http://:4000/rest/events/location/9.59969,52.42556-9.82954,52.33702/since/2017-12-05T19:30:00/until/2017-12-05T19:30:00/type/null
 }
