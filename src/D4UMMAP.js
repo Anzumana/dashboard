fbda2317f7db33c96f0fab88bbb4d96f3d3dd4ae
Anzumana Taal <anzumana.taal@me.com>
2019-11-22 11:27:47 +0100

    feat: app bar , fullscreenButton and styling D4UM-2526

--- src/D4UMMAP.js
+++ src/D4UMMAP.js
@@ -14,7 +14,14 @@ import { splitCamelCase } from './lib/utils';
 import Play from './components/Play';
 import SelectCity from './components/SelectCity.js';
 import FullscreenButton from './components/FullscreenButton.js';
-
+//Material UI Components
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 class D4UMMAP extends Component {
 
 	constructor(props) {
@@ -25,11 +32,6 @@ class D4UMMAP extends Component {
 		};
 		this.test = this.test.bind(this);
 		console.log(this.props.traffic.features);
-		console.log(' HERE IS OUR CONSTRUICTOR');
-
-
-
-	
 	}
 	onDragEnd(result) {
 		console.log(result);
@@ -681,18 +683,6 @@ class D4UMMAP extends Component {
 	}
   render() {
 		const {viewport} = this.state;
-					const buttons = [
-						"streets",
-						"outdoors",
-						"light",
-						"dark",
-						"satellite",
-						"satelliteStreets",
-						"navigationPreviewDay",
-						"navigationPreviewNight",
-						"navigationGuidanceDay",
-						"navigationGuidanceNight",
-					];
 		const activeOptions = this.props.layers.columns.activeOptions.taskIds;
 		if(this.props.fullscreen){
 			return(
@@ -710,6 +700,16 @@ class D4UMMAP extends Component {
 		}
 			return (
 			<div>
+				<AppBar position="static">
+					<Toolbar>
+						<IconButton edge="start">
+							<MenuIcon className="AppBar__button"/>
+						</IconButton>
+						<Typography variant="h6">
+							Data4UrbanMobility Dashboard
+						</Typography>
+					</Toolbar>
+				</AppBar>
 				<FullscreenButton fullscreen={this.props.fullscreen} />
 				<MapGL 
 					{...viewport}
