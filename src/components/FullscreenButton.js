fe1f51063f81c250a39c3569a13f1369c0edaa5e
Anzumana Taal <anzumana.taal@me.com>
2019-11-22 11:31:03 +0100

    refactor: fullscreenButton D4UM-2526

--- src/components/FullscreenButton.js
+++ src/components/FullscreenButton.js
@@ -4,6 +4,10 @@ import store from '../store';
 import FullscreenIcon  from '@material-ui/icons/Fullscreen';
 import FullscreenExitIcon from '@material-ui/icons/FullscreenExit';
 import Button from '@material-ui/core/Button';
+import { theme } from '../lib/theme'; 
+import { ThemeProvider } from '@material-ui/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 export default class FullscreenButton extends React.Component {
 	constructor(props){
@@ -17,24 +21,17 @@ export default class FullscreenButton extends React.Component {
 		if(this.props.fullscreen){
 			return (
 				<div className="FullscreenButton" onClick={() => this.fullscreen()}>
-				<Button
-					variant="contained"
-					endIcon={<FullscreenExitIcon />}
-				>
-					Exit Fullscreen
-				</Button>
+					<Fab size="small" color="secondary" variant="extended" aria-label="fullscreen" >
+						<FullscreenExitIcon fontSize="large"/>
+					</Fab>
 				</div>
 			)
 		}else {
 			return (
 			<div className="FullscreenButton" onClick={() => this.fullscreen()}>
-				<Button
-					variant="contained"
-				size="large"
-					endIcon={<FullscreenIcon fontSize="large"/>}
-				>
-					Enter Fullscreen
-				</Button>
+					<Fab size="medium" variant="extended" color="secondary"aria-label="fullscreen" >
+						<FullscreenIcon fontSize="large"/>
+					</Fab>
 			</div>
 			)
 		}
